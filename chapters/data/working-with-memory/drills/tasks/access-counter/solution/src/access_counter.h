9f7167fe955bbaed1a63674661a14c97b9a8e599
roby405 <robep2341@gmail.com>
2026-01-17 10:28:12 +0200

    removed useless

--- chapters/data/working-with-memory/drills/tasks/access-counter/solution/src/access_counter.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-
-#ifndef ACCESS_PRINTER_H_
-#define ACCESS_PRINTER_H_	1
-
-extern unsigned long counter;
-void register_sigsegv_handler(void);
-
-#endif
--- content/assignments/parallel-graph/tests/grade.sh
+++
@@ -1,1 +0,0 @@
-../../../common/projects/grade.sh
